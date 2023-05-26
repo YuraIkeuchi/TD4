ec6076198a54fa495ebaaccedcf279c27c2d53cc
IkeuchiYura <k020g1233@g.neec.ac.jp>
2023-05-26 10:22:21 +0900

    勝手にカメラ変えちゃいました。すみません

--- SourceCode/gamesystem/gamecamera/CameraWork.h
+++ SourceCode/gamesystem/gamecamera/CameraWork.h
@@ -7,6 +7,11 @@
 #include "InterBoss.h"
 using namespace std;         //  名前空間指定
 
+enum CameraState {
+	CAMERA_NORMAL,
+	CAMERA_LOAD,
+	CAMERA_BOSSDEAD
+};
 //ゲームのカメラの動き
 class CameraWork {
 private:
@@ -17,23 +22,28 @@ private:
 	using XMVECTOR = DirectX::XMVECTOR;
 	using XMMATRIX = DirectX::XMMATRIX;
 
+private:
+	static void (CameraWork::* stateTable[])();
 public:
 	CameraWork(XMFLOAT3 eye = { 2.0f, 30.0f, 2.0f }, XMFLOAT3 target = { 2.0f, 0.0f, 3.0f });
 	void Update(DebugCamera* camera);//更新
 	void ImGuiDraw();
-	void LoadActorUpdate(DebugCamera* camera);//更新
 	void DefaultCam();
-	void SetBossCam(InterBoss* boss);
+	void SetBossCam();
+
+	void SetBoss(InterBoss* boss) { this->boss = boss; }
 	//ゲームシーン以外で使うカメラ更新(this変数で変更可能)
-	void SpecialUpdate(DebugCamera* camera);//更新
+	void SpecialUpdate();//更新
 public:
 	void EditorCamera();
 public:
 	//getter setter
-	void SetEye(const XMFLOAT3 eye) { m_eyePos = eye; }
-	XMFLOAT3 GetEye() { return m_eyePos; }
-	void SetTarget(const XMFLOAT3 target) { m_targetPos = target; }
-	XMFLOAT3 GetTarget() { return m_targetPos; }
+	void SetEye(const XMFLOAT3& eye) { m_eyePos = eye; }
+	XMFLOAT3& GetEye() { return m_eyePos; }
+	void SetTarget(const XMFLOAT3& target) { m_targetPos = target; }
+	XMFLOAT3& GetTarget() { return m_targetPos; }
+
+	void SetCameraState(const int CameraState) { m_CameraState = CameraState; }
 private:
 	//クラス
 	unique_ptr<Shake> shake = nullptr;
@@ -45,4 +55,8 @@ private:
 	XMFLOAT3 m_eyePos = { 0.0f,0.0f,0.0f };
 	//注視点座標
 	XMFLOAT3 m_targetPos = { 0.0f,0.0f,0.0f };
+
+	InterBoss* boss = nullptr;
+
+	int m_CameraState = 0;
 };
