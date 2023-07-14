9f42581e1a34988d0fc8f6b9bca874b9ca355cd6
shimadatuyoshi <k020g1186@g.neec.ac.jp>
2023-07-14 16:52:17 +0900

    突進の予兆範囲追加

--- SourceCode/gamesystem/actor/FirstStageActor.cpp
+++ SourceCode/gamesystem/actor/FirstStageActor.cpp
@@ -68,24 +68,24 @@ void FirstStageActor::Update(DirectXCommon* dxCommon, DebugCamera* camera, Light
 	}
 		sceneChanger_->Update();
 
-	////プレイヤー
-	//if (enemymanager->BossDestroy() && camerawork->GetFeedEnd()) {
-	//	SceneSave::GetInstance()->SetClearFlag(kFirstStage, true);
-	//	lightgroup->SetCircleShadowDir(0, XMVECTOR({ circleShadowDir[0], circleShadowDir[1], circleShadowDir[2], 0 }));
-	//	lightgroup->SetCircleShadowCasterPos(0, XMFLOAT3({ Player::GetInstance()->GetPosition().x, 0.0f, Player::GetInstance()->GetPosition().z }));
-	//	lightgroup->SetCircleShadowAtten(0, XMFLOAT3(circleShadowAtten));
-	//	lightgroup->SetCircleShadowFactorAngle(0, XMFLOAT2(circleShadowFactorAngle));
-	//}
-	//else {//ボス撃破ムービーの後は丸影消す
-	//	lightgroup->SetCircleShadowActive(0, false);
-	//}
+	//プレイヤー
+	if (enemymanager->BossDestroy() && camerawork->GetFeedEnd()) {
+		SceneSave::GetInstance()->SetClearFlag(kFirstStage, true);
+		lightgroup->SetCircleShadowDir(0, XMVECTOR({ circleShadowDir[0], circleShadowDir[1], circleShadowDir[2], 0 }));
+		lightgroup->SetCircleShadowCasterPos(0, XMFLOAT3({ Player::GetInstance()->GetPosition().x, 0.0f, Player::GetInstance()->GetPosition().z }));
+		lightgroup->SetCircleShadowAtten(0, XMFLOAT3(circleShadowAtten));
+		lightgroup->SetCircleShadowFactorAngle(0, XMFLOAT2(circleShadowFactorAngle));
+	}
+	else {//ボス撃破ムービーの後は丸影消す
+		lightgroup->SetCircleShadowActive(0, false);
+	}
 
-	////ボス
-	//lightgroup->SetCircleShadowDir(1, XMVECTOR({ BosscircleShadowDir[0], BosscircleShadowDir[1], BosscircleShadowDir[2], 0 }));
-	//lightgroup->SetCircleShadowCasterPos(1, XMFLOAT3({ enemymanager->GetBoss()->GetPosition().x, 	0.0f, 	enemymanager->GetBoss()->GetPosition().z }));
-	//lightgroup->SetCircleShadowAtten(1, XMFLOAT3(BosscircleShadowAtten));
-	//lightgroup->SetCircleShadowFactorAngle(1, XMFLOAT2(BosscircleShadowFactorAngle));
-	//lightgroup->Update();
+	//ボス
+	lightgroup->SetCircleShadowDir(1, XMVECTOR({ BosscircleShadowDir[0], BosscircleShadowDir[1], BosscircleShadowDir[2], 0 }));
+	lightgroup->SetCircleShadowCasterPos(1, XMFLOAT3({ enemymanager->GetBoss()->GetPosition().x, 	0.0f, 	enemymanager->GetBoss()->GetPosition().z }));
+	lightgroup->SetCircleShadowAtten(1, XMFLOAT3(BosscircleShadowAtten));
+	lightgroup->SetCircleShadowFactorAngle(1, XMFLOAT2(BosscircleShadowFactorAngle));
+	lightgroup->Update();
 
 	Menu::GetIns()->Upda();
 	postEffect->SetCloseRad(Menu::GetIns()->GetCloseIconRad());
