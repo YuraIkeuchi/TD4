60dfb5cefc04e1bb7d011d6d82b1dc83c9449d9c
IkeuchiYura <k020g1233@g.neec.ac.jp>
2023-07-21 16:14:36 +0900

    ダークコトコにパーティクルを追加(改善の余地あり)

--- SourceCode/engine/2d/ImageManager.cpp
+++ SourceCode/engine/2d/ImageManager.cpp
@@ -98,7 +98,6 @@ void ImageManager::SecondLoadTex2D()
 	IKETexture::LoadTexture(ANGER, L"Resources/2d/objicon/AngerStamp.png");
 	IKETexture::LoadTexture(JOY, L"Resources/2d/objicon/JoyStamp.png");
 	IKETexture::LoadTexture(DAMAGEAREA, L"Resources/2d/effect/DArea.png");
-	IKETexture::LoadTexture(DAMAGEAREA2, L"Resources/2d/effect/DArea2.png");
 	IKETexture::LoadTexture(GROUND, L"Resources/2d/SceneBack/Ground.png");
 	IKETexture::LoadTexture(PHOTOSPOT, L"Resources/2d/effect/PhotoSpot.png");
 
